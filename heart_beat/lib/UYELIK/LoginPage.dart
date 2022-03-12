bcf4bc077573adb8c77d0a858abf3248d602544d
YusaCan <ahmedyusacan@gmail.com>
2022-03-12 22:18:40 +0300

    Updated SignIn Page

--- heart_beat/lib/UYELIK/LoginPage.dart
+++ heart_beat/lib/UYELIK/LoginPage.dart
@@ -8,41 +8,43 @@ import '../MainPages/MainMenu.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class UserDataListState extends State<UserDataList>{
+class UserDataListState extends State<UserDataList> {
+  TextEditingController _userPasswordController = TextEditingController();
+  TextEditingController _userEmailController = TextEditingController();
+  TextEditingController _userNameController = TextEditingController();
 
-TextEditingController _userPasswordController = TextEditingController();
-TextEditingController _userEmailController = TextEditingController();
-TextEditingController _userNameController = TextEditingController();
-
-@override
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SignUp(),
     );
   }
 
-bool _canSendUserData() => _userNameController.text.length > 0 && _userPasswordController.text.length > 0 && _userEmailController.text.length > 0;
+  bool _canSendUserData() =>
+      _userNameController.text.length > 0 &&
+      _userPasswordController.text.length > 0 &&
+      _userEmailController.text.length > 0;
 
-void _sendUserData() {
-  if (_canSendUserData()) {
-    final userData = USER_DATA(_userNameController.text,_userEmailController.text,_userPasswordController.text);
-    widget.userdataDAO.saveUserData(userData);
-    _userNameController.clear();
-    _userEmailController.clear();
-    _userPasswordController.clear();
-    setState(() {});
+  void _sendUserData() {
+    if (_canSendUserData()) {
+      final userData = USER_DATA(_userNameController.text,
+          _userEmailController.text, _userPasswordController.text);
+      widget.userdataDAO.saveUserData(userData);
+      _userNameController.clear();
+      _userEmailController.clear();
+      _userPasswordController.clear();
+      setState(() {});
+    }
   }
 }
 
-}
-
-class UserDataList extends StatefulWidget{
-  UserDataList({Key? key}): super(key: key);
+class UserDataList extends StatefulWidget {
+  UserDataList({Key? key}) : super(key: key);
 
   final userdataDAO = USER_DATA_DAO();
 
   @override
-  UserDataListState createState()=> UserDataListState();
+  UserDataListState createState() => UserDataListState();
 }
 
 class SignUp extends StatefulWidget {
@@ -53,7 +55,6 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-
   TextEditingController _userPasswordController = TextEditingController();
   TextEditingController _userEmailController = TextEditingController();
   TextEditingController _userNameController = TextEditingController();
@@ -73,17 +74,19 @@ class _SignUpState extends State<SignUp> {
   }
 
   Future<void> _loginUser() async {
-    try{
+    try {
       print("user:  $email");
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email.trim(), password: _userPasswordController.text);
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: email.trim(), password: _userPasswordController.text);
       print("user:  $userCredential");
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => MainMenuPage()),
       );
-    } on FirebaseAuthException catch (e){
+    } on FirebaseAuthException catch (e) {
       print("error: $e");
-    }catch(e){
+    } catch (e) {
       print("error: $e");
     }
   }
@@ -92,10 +95,11 @@ class _SignUpState extends State<SignUp> {
 
   Future<void> signup(BuildContext context) async {
     final GoogleSignIn googleSignIn = GoogleSignIn();
-    final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
     if (googleSignInAccount != null) {
       final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+          await googleSignInAccount.authentication;
       final AuthCredential authCredential = GoogleAuthProvider.credential(
           idToken: googleSignInAuthentication.idToken,
           accessToken: googleSignInAuthentication.accessToken);
@@ -111,6 +115,7 @@ class _SignUpState extends State<SignUp> {
       // for go to the HomePage screen
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -173,7 +178,7 @@ class _SignUpState extends State<SignUp> {
               padding: EdgeInsets.symmetric(horizontal: 45, vertical: 8),
               child: TextFormField(
                 obscureText: seePassword,
-                controller: _userPasswordController,//bu password
+                controller: _userPasswordController, //bu password
                 decoration: InputDecoration(
                   suffixIcon: IconButton(
                     onPressed: () {
@@ -187,56 +192,79 @@ class _SignUpState extends State<SignUp> {
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 100, vertical: 8),
-              child: Wrap(
-                children: <Widget>[
-                  CheckboxListTile(
-                      title: Text("Remember me"),
-                      controlAffinity: ListTileControlAffinity.leading,
-                      value: checkBoxValue,
-                      activeColor: Colors.green,
-                      onChanged:(newValue){
-                        setState(() {
-                          checkBoxValue = newValue!;
-                        });
-                      }
-                  ),
-                ],
-              ),
-            ),
-            // Sign Up Button
-            Padding(
-                padding: EdgeInsets.symmetric(horizontal: 110, vertical: 10),
-                child: Row(
-                  children: <Widget>[
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Color.fromARGB(255, 5, 115, 218),
-                          padding: EdgeInsets.symmetric(horizontal: 70, vertical: 10),
-                          textStyle: TextStyle(fontSize: 20,)),
-                      child: Text('Login'),
-                      onPressed: () {
-                        _loginUser();
-                        // When tapped, if everything is right, passed to the Main Menu Page. It needs an animation between two pages.
-                      }),
-                  ],
+            Row(
+              children: <Widget>[
+                Padding(padding: EdgeInsets.only(left: 190)),
+                Checkbox(
+                  value: checkBoxValue,
+                  activeColor: Colors.green,
+                  onChanged: (newValue) {
+                    setState(() {
+                      checkBoxValue = newValue!;
+                    });
+                  },
                 ),
+                Text(
+                  "Remember me",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
             ),
+
+            // Sign Up Button
+            Row(
+              children: <Widget>[
+                Padding(padding: EdgeInsets.only(left: 87)),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(255, 5, 115, 218),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                        )),
+                    child: Text('Login'),
+                    onPressed: () {
+                      _loginUser();
+                      // When tapped, if everything is right, passed to the Main Menu Page. It needs an animation between two pages.
+                    }),
+              ],
+            ),
+
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
-              child: Wrap(
+              padding: EdgeInsets.only(left: 49),
+              child: Row(
                 children: <Widget>[
-                  Text("Don't have an account? Sign Up!",
+                  Padding(padding: EdgeInsets.only(left: 35)),
+                  Text(
+                    "Don't have an account? ",
                     style: TextStyle(
-                    color: Colors.blueAccent,
-                  ),),
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                    child: Text(
+                      "Sign Up!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blueAccent,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 100, vertical: 5),
-
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -244,13 +272,15 @@ class _SignUpState extends State<SignUp> {
                     icon: Image.asset('assets/1534129544.png'),
                     iconSize: 50,
                     onPressed: () {
-                      signup(context);},
+                      signup(context);
+                    },
                   ),
                   IconButton(
                     icon: Image.asset('assets/Facebook.png'),
                     iconSize: 50,
                     onPressed: () {
-                      signup(context);},
+                      signup(context);
+                    },
                   ),
                 ],
               ),
